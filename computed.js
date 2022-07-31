43b5f1aff8f35bea122cf2d4afd33aacfaae53c7
Ayu Nakata <mieze018@users.noreply.github.com>
2022-07-31 14:03:49 +0900

    [computed]単価と個数を入力したら税込の金額を計算する

--- computed.js
+++ computed.js
@@ -2,10 +2,17 @@
 new Vue({
   el: "#app",
   data: {
-    price: 100
+    price: 100,
+    count: 1
   },
   computed: {
     //priceが変わったら消費税込み金額を算出する
+    sum: function () {
+      return this.price * this.count;
+    },
+    taxIncludedSum: function () {
+      return this.sum * 1.08;
+    },
     taxIncluded: function () {
       return this.price * 1.08;
     },
