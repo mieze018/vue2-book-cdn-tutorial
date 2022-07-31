018c9e2d6ea6464e403d479444080b980fdaa044
Ayu Nakata <mieze018@users.noreply.github.com>
2022-07-31 14:20:38 +0900

    [computed]文字を入力したら残りの入力可能文字数を表示する

--- computed.js
+++ computed.js
@@ -3,9 +3,28 @@ new Vue({
   el: "#app",
   data: {
     price: 100,
-    count: 1
+    count: 1,
+    myText: 'こんにちは',
+    //入力可能文字数
+    maxLength: 140,
   },
   computed: {
+    //myTextの長さが変わったら、残りの文字数を算出する
+    remainText: function () {
+      return this.maxLength - this.myText.length;
+    },
+    //remainTextが変わったら、色を変える
+    remainColor: function () {
+      color = "green";
+      if (this.remainText < 20) {
+        color = "orange";
+      }
+      if (this.remainText < 1) {
+        color = "red";
+      }
+      return color;
+    },
+
     //priceが変わったら消費税込み金額を算出する
     sum: function () {
       return this.price * this.count;
