52c77c10cf5a9de19d47fe03eda635e34b46d545
Ayu Nakata <mieze018@users.noreply.github.com>
2022-07-31 15:07:10 +0900

    [computed]インクリメントサーチ

--- computed.js
+++ computed.js
@@ -1,14 +1,38 @@
-
 new Vue({
   el: "#app",
   data: {
     price: 100,
     count: 1,
-    myText: 'こんにちは',
+    myText: "こんにちは",
     //入力可能文字数
     maxLength: 140,
+    findWord: "",
+    items: [
+      "OptimusPrime",
+      "BumbleBee",
+      "IronHide",
+      "Prowl",
+      "Jazz",
+      "WheelJack",
+      "GrimLock",
+      "Megatron",
+      "StarScream",
+      "SoundWave",
+      "LaserWave",
+      'AstroTrain',
+      'BlitzWing',
+    ],
   },
   computed: {
+    //this.findWordが変わったらその文字が含まれるアイテムを探す
+    foundItems: function () {
+      if (this.findWord)
+        return this.items.filter(function (item) {
+          //大文字小文字を区別しない
+          return item.toLowerCase().indexOf(this.findWord.toLowerCase()) > -1;
+        }, this);
+      return this.items;
+    },
     //myTextの長さが変わったら、残りの文字数を算出する
     remainText: function () {
       return this.maxLength - this.myText.length;
